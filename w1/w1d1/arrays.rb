90b1d7cce5e663582d601b3afe4f70be36885dda
vveleva <vvveleva@gmail.com>
2014-12-26 21:18:29 -0800

    Separate towers of hanoi

--- w1/w1d1/arrays.rb
+++ w1/w1d1/arrays.rb
@@ -20,28 +20,6 @@ class Array
 end
 
 
-tower = { 1 => [4, 3, 2, 1], 2 => [], 3 => [] }
-loop do
-  tower.values.each { |stack| p stack }
-  p 'Move from:'
-  initial_tower = tower[gets.chomp.to_i]
-  if initial_tower.empty?
-    p 'Tower is empty'
-    next
-  end
-  p 'Move to:'
-  destination_tower = tower[gets.chomp.to_i]
-  unless initial_tower.empty?
-    if destination_tower.empty? || initial_tower.last < destination_tower.last
-      destination_tower << initial_tower.pop
-      p 'You win' && break if destination_tower == [4, 3, 2, 1]
-    else
-      p 'Illegal move'
-    end
-  end
-end
-
-
 def my_transpose(matrix)
   result = []
   len = matrix[0].length
@@ -50,6 +28,7 @@ def my_transpose(matrix)
     matrix.each { |row| col << row[i] }
     result << col
   end
+
   result
 end
 
@@ -60,8 +39,8 @@ def stock_picker(arr)
   (0...len - 1).each do |i|
     (i + 1...len).each do |j|
       profit = [i, j] if arr[j] - arr[i] > arr[profit[1]] - arr[profit[0]]
-      p profit
     end
   end
+
   profit
 end
---
+++ w1/w1d1/towers_of_hanoi.rb
@@ -0,0 +1,78 @@
+require 'colorize'
+
+class TowersOfHanoi
+
+  attr_reader :towers, :complete_tower
+
+  def initialize
+    @complete_tower = (1..5).to_a.reverse
+    @towers = [complete_tower.dup, [], []]
+  end
+
+  def play
+    display
+    until won?
+      begin
+        p 'Move from, to:'
+        from, to = get_move(gets.chomp)
+      rescue ArgumentError => e
+        puts e.message
+        retry
+      end
+      next if illegal_move?(from, to)
+
+      display
+    end
+  end
+
+  def won?
+    if towers.last == complete_tower
+      puts 'You win!'
+      true
+    else
+      false
+    end
+  end
+
+  def get_move(input)
+    input = input.split(",").map(&:strip).map(&:to_i)
+    arr = [1, 2, 3]
+    unless arr.include?(input[0]) && arr.include?(input[1])
+      raise ArgumentError.new "invalid input"
+    end
+
+    input.map { |i| i - 1 }
+  end
+
+  def illegal_move?(from, to)
+    if towers[from].empty?
+      p 'Tower is empty'
+      true
+    elsif towers[to].empty? || towers[from].last < towers[to].last
+      towers[to] << towers[from].pop
+      false
+    else
+      p 'Illegal move'
+      true
+    end
+  end
+
+  def render
+    (complete_tower.length - 1).downto(0).map do |height|
+      towers.map do |stack|
+        if stack[height].nil?
+          "\u2551"
+        else
+          stack[height].to_s.colorize(:red)
+        end
+      end.join("\t")
+    end.join("\n")
+  end
+
+  def display
+    puts "\e[H\e[2J"
+    puts render
+    puts "\u2569" + "\u2550" * 7 + "\u2569" + "\u2550" * 7 + "\u2569"
+  end
+
+end
