9703108beee86d32248ada6e086b89e7452060a1
vveleva <vvveleva@gmail.com>
2015-03-20 15:35:44 -0700

    Clean up code

--- w5/w5d5/towers_of_hanoi.js
+++ w5/w5d5/towers_of_hanoi.js
@@ -6,24 +6,20 @@ function TowersOfHanoi() {
   this.stacks = [[3, 2, 1], [], []];
 };
 
-TowersOfHanoi.prototype.isWon = function() {
-  if (this.stacks[2].length === 3 ) {
-    reader.close;
-    return true;
-  } else {
-    return false;
-  }
-};
-
 function last(array) { return array[array.length - 1] };
 
-TowersOfHanoi.prototype.isValidMove = function (startTowerIdx, endTowerIdx) {
+TowersOfHanoi.prototype.isWon = function() {
+  return this.stacks[2].length === 3;
+};
 
-  if (this.stacks[startTowerIdx].length === 0) {
+TowersOfHanoi.prototype.isValidMove = function (startTowerIdx, endTowerIdx) {
+  var startStack = this.stacks[startTowerIdx];
+  var endStack   = this.stacks[endTowerIdx];
+  if (startStack.length === 0) {
     return false;
-  } else if (this.stacks[endTowerIdx].length === 0) {
+  } else if (endStack.length === 0) {
     return true;
-  } else if (last(this.stacks[startTowerIdx]) < last(this.stacks[endTowerIdx])) {
+  } else if (last(startStack) < last(endStack)) {
     return true;
   } else {
     return false;
@@ -55,16 +51,10 @@ TowersOfHanoi.prototype.promptMove = function (callback) {
 
 TowersOfHanoi.prototype.run = function (callback) {
   this.print();
-  var that = this;
   this.promptMove(function(from, to) {
-    that.move(from, to);
-    if (that.isWon()) {
-
-      callback();
-    } else {
-      that.run(callback);
-    }
-  })
+    this.move(from, to);
+    this.isWon() ? callback() : this.run(callback);
+  }.bind(this));
 };
 
 a = new TowersOfHanoi();
@@ -72,8 +62,3 @@ a.run(function(){
   console.log("You're done!")
   reader.close();
 });
-
-
-
-
-// stacks[0][stacks.length - 1]
