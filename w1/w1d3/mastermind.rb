c94bf405e85b61b2f2cc1513a2b2ee080c9dfe3c
vveleva <vvveleva@gmail.com>
2015-04-22 17:55:11 -0700

    Use code object instead of its pegs

--- w1/w1d3/mastermind.rb
+++ w1/w1d3/mastermind.rb
@@ -19,8 +19,9 @@ class Code
 
   def exact_matches(other_code)
     matches = 0
+    other_pegs = other_code.pegs
     pegs.each_index do |i|
-      matches += 1 if pegs[i] == other_code[i]
+      matches += 1 if pegs[i] == other_pegs[i]
     end
 
     matches
@@ -28,7 +29,7 @@ class Code
 
   def near_matches(other_code)
     matches = 0
-    other_code.each do |color|
+    other_code.pegs.each do |color|
       matches += 1 if pegs.include?(color)
     end
 
@@ -51,22 +52,22 @@ class Game
     until over?
       self.guess = get_guess
       self.turns += 1
-      exact = secret_code.exact_matches(guess.pegs)
-      near = secret_code.near_matches(guess.pegs)
+      exact = secret_code.exact_matches(guess)
+      near = secret_code.near_matches(guess)
       puts "Exact: #{exact}"
       puts "Near: #{near}"
       puts "You have #{10 - turns} left."
     end
 
-    puts "The secret code was #{secret_code.pegs.join('')}!"
+    puts "\nThe secret code was #{secret_code.pegs.join('')}!"
   end
 
   def over?
-    if guess && secret_code.exact_matches(guess.pegs) == 4
-      puts "you win!"
+    if guess && secret_code.exact_matches(guess) == 4
+      puts "\nYou win!"
       true
     elsif turns == 10
-      puts "you lose!"
+      puts "\nYou lose!"
       true
     else
       false
